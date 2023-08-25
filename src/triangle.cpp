0b143a76816223dceb0b5c26e4d1cd31b9d9ef1f
Aziz <azizbahri90@gmail.com>
2023-08-25 22:24:49 +0100

    check that we have a valid swapchain
    Signed-off-by: Aziz <azizbahri90@gmail.com>

--- src/triangle.cpp
+++ src/triangle.cpp
@@ -139,7 +139,6 @@ private:
         window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
     }
 
-
     // create a surface to draw to
     void createSurface()
     {
@@ -254,6 +253,7 @@ private:
      * has a graphics queue family
      * has a present queue family
      * has the required extensions
+     * has a swap chain that is adequate
      */
     bool isDeviceSuitable(VkPhysicalDevice device)
     {
@@ -267,9 +267,41 @@ private:
         vkGetPhysicalDeviceProperties(device, &deviceProperties);
         vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
 
+        bool swapChainAdequate = false;
+        if (extensionsSupported) {
+            SwapChainSupportDetails swapChainSupport = querySwapChainSupport(device);
+            swapChainAdequate = !swapChainSupport.formats.empty() && !swapChainSupport.presentModes.empty();
+        }
+
         return (deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU &&
                 deviceFeatures.geometryShader) &&
-               indices.isComplete() && extensionsSupported;
+               indices.isComplete() && extensionsSupported && swapChainAdequate;
+    }
+
+    SwapChainSupportDetails querySwapChainSupport(VkPhysicalDevice device)
+    {
+        SwapChainSupportDetails details;
+
+        vkGetPhysicalDeviceSurfaceCapabilitiesKHR(device, surface, &details.capabilities);
+
+        uint32_t formatCount;
+        vkGetPhysicalDeviceSurfaceFormatsKHR(device, surface, &formatCount, nullptr);
+
+        if (formatCount != 0)
+        {
+            details.formats.resize(formatCount);
+            vkGetPhysicalDeviceSurfaceFormatsKHR(device, surface, &formatCount, details.formats.data());
+        }
+
+        uint32_t presentModeCount;
+        vkGetPhysicalDeviceSurfacePresentModesKHR(device, surface, &presentModeCount, nullptr);
+
+        if (presentModeCount != 0)
+        {
+            details.presentModes.resize(presentModeCount);
+            vkGetPhysicalDeviceSurfacePresentModesKHR(device, surface, &presentModeCount, details.presentModes.data());
+        }
+        return details;
     }
 
     bool checkDeviceExtensionSupport(VkPhysicalDevice device)
