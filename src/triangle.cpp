44efefafc7b57979daf8fd46f231005cb909d51b
Aziz <azizbahri90@gmail.com>
2023-09-10 08:41:21 +0100

    create framebuffers
    Signed-off-by: Aziz <azizbahri90@gmail.com>

--- src/triangle.cpp
+++ src/triangle.cpp
@@ -143,6 +143,7 @@ private:
 
     // swap chain image views
     std::vector<VkImageView> swapChainImageViews;
+    std::vector<VkFramebuffer> swapChainFramebuffers;
 
     // graphics pipeline
     VkPipelineLayout pipelineLayout;
@@ -151,6 +152,7 @@ private:
     VkRenderPass renderPass;
 
     VkPipeline graphicsPipeline;
+
     void initWindow()
     {
         // initialize glfw
@@ -914,6 +916,31 @@ private:
             throw std::runtime_error("failed to create render pass!");
         }
     }
+
+    void createFramebuffers()
+    {
+        swapChainFramebuffers.resize(swapChainImageViews.size());
+
+        for (size_t i = 0; i < swapChainImageViews.size(); i++)
+        {
+            VkImageView attachments[] = {
+                swapChainImageViews[i]};
+
+            VkFramebufferCreateInfo framebufferInfo{};
+            framebufferInfo.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
+            framebufferInfo.renderPass = renderPass;
+            framebufferInfo.attachmentCount = 1;
+            framebufferInfo.pAttachments = attachments;
+            framebufferInfo.width = swapChainExtent.width;
+            framebufferInfo.height = swapChainExtent.height;
+            framebufferInfo.layers = 1;
+
+            if (vkCreateFramebuffer(device, &framebufferInfo, nullptr, &swapChainFramebuffers[i]) != VK_SUCCESS)
+            {
+                throw std::runtime_error("failed to create framebuffer!");
+            }
+        }
+    }
     void initVulkan()
     {
         // create a vulkan instance
@@ -942,10 +969,18 @@ private:
 
         // create graphics pipeline
         createGraphicsPipeline();
+
+        // create the framebuffers
+        createFramebuffers();
     }
 
     void cleanup()
     {
+        for (auto framebuffer : swapChainFramebuffers)
+        {
+            vkDestroyFramebuffer(device, framebuffer, nullptr);
+        }
+
         vkDestroyPipeline(device, graphicsPipeline, nullptr);
         vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
         vkDestroyRenderPass(device, renderPass, nullptr);
